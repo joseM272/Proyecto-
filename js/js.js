f59aed298779befbe4492e713cf66f39e340100d
joseM272 <joseignaciomontoya22@gmail.com>
2025-10-22 19:11:52 -0300

    const cambiarTema en js

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@ console.error("");
 console.debug(document);
 console.info(document);
 console.trace(document);
-const temaOscuto = () => {
+const temaOscuro = () => {
     document.querySelector("body").setAttribute("data-bs-theme", "dark");
     document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun fill");
 }
@@ -13,4 +13,8 @@ const temaOscuto = () => {
 const temaClaro = () => {
     document.querySelector("body").setAttribute("data-bs-theme", "light");
     document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon fill");
+}
+const cambiarTema = () => {
+    document.querySelector("body").getAttribute("data-bs-theme") === "dark"?
+    temaOscuro() : temaClaro();
 }
