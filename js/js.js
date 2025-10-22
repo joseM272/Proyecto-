b6a5881590a7390733d70649fe0acb6afab49c87
joseM272 <joseignaciomontoya22@gmail.com>
2025-10-22 19:08:17 -0300

    const temaClaro en js

--- js/js.js
+++ js/js.js
@@ -9,3 +9,8 @@ const temaOscuto = () => {
     document.querySelector("body").setAttribute("data-bs-theme", "dark");
     document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun fill");
 }
+
+const temaClaro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon fill");
+}
