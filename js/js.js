1ed90b45d6054f96a986ce929f2e2c7895378e90
joseM272 <joseignaciomontoya22@gmail.com>
2025-10-22 18:53:49 -0300

    Creando un (Button) con forma de luna

---
+++ js/js.js
@@ -0,0 +1,7 @@
+
+console.log("Brisa va a desaprobar");
+console.warn("Facu esta apunto");
+console.error("julian desaprobo");
+console.debug(document);
+console.info(document);
+console.trace(document);
