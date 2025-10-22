3d7a50d3da5340fa811f31985f4749138180dbec
joseM272 <joseignaciomontoya22@gmail.com>
2025-10-22 19:00:49 -0300

    JS, agregando const temaOscuro

--- js/js.js
+++ js/js.js
@@ -1,7 +1,10 @@
 
-console.log("Brisa va a desaprobar");
-console.warn("Facu esta apunto");
-console.error("julian desaprobo");
+console.log("Cierren bien las etiquetas");
+console.warn("");
+console.error("");
 console.debug(document);
 console.info(document);
 console.trace(document);
+const temaOscuto = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark")
+}
