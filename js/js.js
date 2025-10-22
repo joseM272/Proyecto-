decd70c60710be28bb30cef08b495c1e15a186eb
joseM272 <joseignaciomontoya22@gmail.com>
2025-10-22 19:16:08 -0300

    Agustando codigo js y corrigiendo id de la etiqueta i

--- js/js.js
+++ js/js.js
@@ -7,14 +7,21 @@ console.info(document);
 console.trace(document);
 const temaOscuro = () => {
     document.querySelector("body").setAttribute("data-bs-theme", "dark");
-    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun fill");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
 }
 
 const temaClaro = () => {
     document.querySelector("body").setAttribute("data-bs-theme", "light");
-    document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon fill");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
 }
+
 const cambiarTema = () => {
-    document.querySelector("body").getAttribute("data-bs-theme") === "dark"?
-    temaOscuro() : temaClaro();
+    // Si el tema actual es 'dark', cambiar a claro; si no, cambiar a oscuro
+    const cuerpo = document.querySelector("body");
+    const temaActual = cuerpo.getAttribute("data-bs-theme");
+    if (temaActual === "dark") {
+        temaClaro();
+    } else {
+        temaOscuro();
+    }
 }
